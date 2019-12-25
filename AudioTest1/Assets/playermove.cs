f26f9ee57a29f995045c3c3beeccc96de988de09
Sulkyoptimism <47511712+Sulkyoptimism@users.noreply.github.com>
2019-12-25 20:22:33 +0000

    Init

---
+++ AudioTest1/Assets/playermove.cs
@@ -0,0 +1,17 @@
+﻿
+using UnityEngine;
+
+public class playermove : MonoBehaviour
+{
+    public CharacterController cont;
+    public float speed = 12f;
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        cont.Move(move*speed*Time.deltaTime);
+    }
+}
